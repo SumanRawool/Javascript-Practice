3850f73ac520f768b2adbcc705faad25712c1a93
Suman <sumanrawool2@gmail.com>
2024-08-28 23:27:06 +0530

    conversion of datatype

--- Basics/conversion.js
+++ Basics/conversion.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+let score = "34";
+
+console.log(typeof score);
+let valueNumer = Number(score);
+console.log(valueNumer);
+console.log(typeof valueNumer);
+
+
+//"33jjj" -> NaN
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// 1 => true 0=>false
+//"" => false "suman" =>true
+
+
+let someNumber = 99
+let someString = String(someNumber);
+console.log(someString)
+console.log(typeof someString);
