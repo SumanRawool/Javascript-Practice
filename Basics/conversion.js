f9337d230456219f0103de6a5eaacbeea4117611
Suman <sumanrawool2@gmail.com>
2024-08-28 23:42:44 +0530

    string to number confusing conversion

--- Basics/conversion.js
+++ Basics/conversion.js
@@ -1,9 +1,9 @@
 let score = "34";
 
-console.log(typeof score);
+// console.log(typeof score);
 let valueNumer = Number(score);
-console.log(valueNumer);
-console.log(typeof valueNumer);
+// console.log(valueNumer);
+// console.log(typeof valueNumer);
 
 
 //"33jjj" -> NaN
@@ -11,12 +11,31 @@ console.log(typeof valueNumer);
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 // 1 => true 0=>false
 //"" => false "suman" =>true
 
 
 let someNumber = 99
 let someString = String(someNumber);
-console.log(someString)
-console.log(typeof someString);
+// console.log(someString)
+// console.log(typeof someString);
+
+
+//============================ Conversion ==================================
+ let value = 3
+ let negValue = - value;
+ //console.log(negValue)
+//console.log(2+2)
+
+let str1 = "hello "
+let str2 = "suman"
+let str3 = str1 + str2
+//console.log(str3)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+1+2)
+// console.log(1+1+"2")
+
+let num1,num2,num3;
+num1=num2=num3=2+2
