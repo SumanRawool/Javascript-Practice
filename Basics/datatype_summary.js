abbac945574b0f19c062d53ceeeb59e1c33fe892
Suman <sumanrawool2@gmail.com>
2024-08-30 22:40:23 +0530

    Datatypes in JavaScript

---
+++ Basics/datatype_summary.js
@@ -0,0 +1,27 @@
+// Primitive
+// 7 Types: String,Number,Boolean,null, undefined,Symbol,BigInt
+
+//JavaScript is dynamically typed language because it data type will automatically assigned at the time of compilation or code execution.
+// const id=Symbol('123');
+// const anotherId = Symbol('123')//both are different bcoz we used Symbol()
+// console.log(id === anotherId);
+
+const bigNumber = 455214556622n
+
+const nullValue = null
+
+//Referaece or non-primitive
+//Arrays, Objects, Functions
+
+// const heros =["shaktiman","naagraj","doga"]
+// let obj = {
+//     name:"suman",
+//     age:22
+// }
+// console.log(typeof obj)//object
+// const myFunction = function (){
+//     console.log("Hello World");   
+// }
+
+//console.log(typeof bigNumber)
+//console.log(typeof nullValue) //object
