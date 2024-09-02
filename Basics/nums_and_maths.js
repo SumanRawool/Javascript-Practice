7143de01d76b83f520a01388c282ed1933cf5964
Suman <sumanrawool2@gmail.com>
2024-09-02 22:37:05 +0530

    working with maths

---
+++ Basics/nums_and_maths.js
@@ -0,0 +1,11 @@
+const score = 400;
+const num = new Number(500);
+// console.log(score);
+// console.log(num);
+
+// console.log(num.toString().length)
+// console.log(num.toFixed(2))
+
+//+++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
+console.log(Math);
+
---
+++ Basics/string.js
@@ -0,0 +1,13 @@
+const name = "suman"
+const repoCount = "12"
+// console.log(name+repoCount+"value")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`) //stringInterpolation
+const myGame = new String('suman-game')
+// console.log(myGame.length);
+// console.log(myGame.toUpperCase());
+// console.log(myGame.charAt(0));
+// console.log(myGame.indexOf('g'));
+
+const newString = myGame.substring(0,4);
+console.log(newString);
